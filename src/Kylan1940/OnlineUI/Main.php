2222b880f3de613a6813d9134820679341d3492f
Kylan1940 <kevingaming123copyright@gmail.com>
2023-07-15 16:13:36 +0700

    remove PluginUpdater

--- src/Kylan1940/OnlineUI/Main.php
+++ src/Kylan1940/OnlineUI/Main.php
@@ -8,25 +8,14 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use Kylan1940\OnlineUI\Commands\OnlineCommand;
-use Kylan1940\OnlineUI\UpdateNotifier\{ConfigUpdater, PluginUpdater};
+use Kylan1940\OnlineUI\UpdateNotifier\ConfigUpdater;
 
 class Main extends PluginBase {
 	
 	public function onEnable() : void {
-	  ConfigUpdater::update($this);
+    ConfigUpdater::update($this);
     $this->getResource("config.yml");
     $this->getServer()->getCommandMap()->register('Online', new OnlineCommand($this));
   }
   
-  /**
-	 * Submits an async task which then checks if a new version for the plugin is available.
-	 * If an update is available then it would print a message on the console.
-	 *
-	 * @param string $pluginName
-	 * @param string $pluginVersion
-	 */
-	public static function checkUpdate(string $pluginName, string $pluginVersion): void {
-		Server::getInstance()->getAsyncPool()->submitTask(new PluginUpdater($pluginName, $pluginVersion));
-	}
-	
 }
